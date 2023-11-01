42d262ef7d1d10b34238784227f966a607867347
binhdv <binh.doan@sunteco.io>
2023-11-01 17:17:47 +0700

    xxx
    Signed-off-by: binhdv <binh.doan@sunteco.io>

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  openIdProviderUrl: 'http://localhost:8080/openid-connect-server-webapp',
-  authorizeEndpoint: 'http://localhost:8080/openid-connect-server-webapp/authorize',
-  tokenEndpoint: 'http://localhost:8080/openid-connect-server-webapp/token',
-  client_id: '06dd89b3-6334-4b91-8989-6b8022e9afa7',
+  openIdProviderUrl: 'http://localhost:9000',
+  authorizeEndpoint: 'http://localhost:9000/oauth2/authorize',
+  tokenEndpoint: 'http://localhost:9000/oauth2/token',
+  client_id: 'client-2',
   callbackPath: '/callback'
 }
